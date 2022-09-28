df9b4c4d6f49c0113adc431c400cde151069d3d8
Eric Button <eric.button@protonmail.com>
2022-09-27 21:32:37 -0500

    Final commit 9.27

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,34 @@
 
 // This code was largely inspired by Dom's tutorial on HW14
 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 function DrawBargraph(sampleId)
 {
-    console.log(`Draw Bar Graph()`);
+    console.log(`Draw Bar Graph(${sampleId})`);
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.out_ids;
+        let otu_labels = result.out_labels;
+        let sample_values = result.sample_values;
+
+
+        let yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuId}`);
+        
+        let barData = {
+            x: sample_values.slice(0,10),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0,10),
+            orientation: 'h'
+        }
+    });
 }
 
 function DrawBubbleChart(sampleId)
@@ -16,13 +41,22 @@ function ShowMetadata(sampleId)
     console.log(`Show MetaData(${sampleId})`);
 }
 
+
+function optionChanged(sampleId)
+{
+    console.log(`optionChanged: ${sampleId}`);
+
+    DrawBargraph(sampleId);
+    DrawBubbleChart(sampleId);
+    ShowMetadata(sampleId);
+}
+
 function InitDashboard()
 {
     console.log("InitDashboard");
     
     let selector = d3.select("#selDataset");
 
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
     d3.json(url).then(data => {
         console.log("Here's the data:", data)
@@ -37,9 +71,15 @@ function InitDashboard()
         };
 
         let initialId = selector.property("value");
-        console.log(`initialId = ${initialId}`)
+        console.log(`initialId = ${initialId}`);
 
-        
+        DrawBargraph(initialId);
+
+        DrawBubbleChart(initialId)
+
+        ShowMetadata(initialId)
+
+        optionChanged(initialId)
 
     });
 }

0bc0992053910dab87609a54df8fdf27c425ccb6
Eric Button <eric.button@protonmail.com>
2022-09-28 14:15:34 -0500

    Finished BarGraph and BubbleChart

--- static/js/app.js
+++ static/js/app.js
@@ -8,32 +8,82 @@ function DrawBargraph(sampleId)
     console.log(`Draw Bar Graph(${sampleId})`);
 
     d3.json(url).then(data => {
-        console.log(data);
 
         let samples = data.samples;
         let resultArray = samples.filter(s => s.id == sampleId);
         let result = resultArray[0];
 
-        let otu_ids = result.out_ids;
-        let otu_labels = result.out_labels;
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
         let sample_values = result.sample_values;
 
-
-        let yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuId}`);
+        let yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
         
+        //Create a trace object
         let barData = {
-            x: sample_values.slice(0,10),
+            x: sample_values.slice(0, 10).reverse(),
             y: yticks,
             type: 'bar',
-            text: otu_labels.slice(0,10),
+            text: otu_labels.slice(0,10).reverse(),
             orientation: 'h'
+        };
+        //Put the trace object into an array
+        let barArray = [barData];
+        //Create a layout object
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
         }
+        //Call the Plotly function
+        Plotly.newPlot("bar", barArray, barLayout);
+
     });
+
+
 }
 
 function DrawBubbleChart(sampleId)
 {
     console.log(`Draw Bubble Chart(${sampleId})`);
+
+    d3.json(url).then(data => {
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
+        
+        //Create a trace object
+        let bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+        //Put the trace object into an array
+        let bubbleArray = [bubbleData];
+        //Create a layout object
+        let bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: {t: 30},
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        }
+        //Call the Plotly function
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout);
+
+    });
+
 }
 
 function ShowMetadata(sampleId)
@@ -64,19 +114,24 @@ function InitDashboard()
         let sampleNames = data.names;
         console.log("Here are the sample names:", sampleNames)
 
+
+        // Populate the dropdown box
         for (let i = 0; i < sampleNames.length; i++) {
             let sampleId = sampleNames[i];
             selector.append("option").text(sampleId).property("value", sampleId);
 
         };
 
+        //Read the current value from the dropdown
         let initialId = selector.property("value");
         console.log(`initialId = ${initialId}`);
-
+        // Draw the bar graph for the selected sample id
         DrawBargraph(initialId);
 
+        // Draw the bubblechart for the selected sample id
         DrawBubbleChart(initialId)
 
+        // Show the metadata for the selected sample id
         ShowMetadata(initialId)
 
         optionChanged(initialId)
